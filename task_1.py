a663d3d9642b2b11a8981ce5fd4f0cf97880bfed
AndyGrigs <97991761+AndyGrigs@users.noreply.github.com>
2025-10-23 17:33:14 +0200

    set up the tast

--- task_1.py
+++ task_1.py
@@ -1,28 +1,24 @@
+# Визначення класу Teacher
+class Teacher:
+    pass
 
+def create_schedule(subjects, teachers):
+   pass
 
-def find_min_max(arr, start=0, end=None ):
+if __name__ == '__main__':
+    # Множина предметів
+    subjects = {}
+    # Створення списку викладачів
+    teachers = []
 
-    if end is None:
-        end = len(arr) - 1
+    # Виклик функції створення розкладу
+    schedule = create_schedule(subjects, teachers)
 
-    if start == end:
-        return (arr[start], arr[end])
-    
-    if end - start == 1:
-        return (min(arr[start], arr[end]), max(arr[start], arr[end]))
-    
-    mid =  (start + end) // 2
-
-    left_min, left_max = find_min_max(arr, start, mid)
-    right_min, right_max = find_min_max(arr, mid + 1, end)
-
-    return (min(left_min, right_min), max(left_max, right_max))
-
-# Тест 1
-print(find_min_max([5, 3, 8, 1, 9, 2, 7]))  # Очікується: (1, 9)
-
-# Тест 2
-print(find_min_max([42]))  # Очікується: (42, 42)
-
-# Тест 3
-print(find_min_max([10, -5]))  # Очікується: (-5, 10)
+    # Виведення розкладу
+    if schedule:
+        print("Розклад занять:")
+        for teacher in schedule:
+            print(f"{teacher.first_name} {teacher.last_name}, {teacher.age} років, email: {teacher.email}")
+            print(f"   Викладає предмети: {', '.join(teacher.assigned_subjects)}\\n")
+    else:
+        print("Неможливо покрити всі предмети наявними викладачами.")
